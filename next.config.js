9e382c0b8073fdd45b420db96ab204940bac5320
Mauro Dion Alonso Olmo <maurodion@gmail.com>
2025-06-23 12:19:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -5,23 +5,20 @@ const webpack = require('webpack');
 const nextConfig = {
   webpack: (config, { isServer }) => {
     // Add this line to enable WebAssembly support
-    config.experiments = { ...config.experiments, asyncWebAssembly: true, layers: true };
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
 
-    // This fix is only applied to the client-side bundle.
-    if (!isServer) {
-      // Handles 'node:process' imports in the browser.
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'node:process': 'process/browser',
-      };
-      
-      // Makes 'process' globally available in the browser, which some libraries expect.
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          process: 'process/browser',
-        })
-      );
-    }
+    // Handles 'node:process' imports in the browser.
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'node:process': 'process/browser',
+    };
+    
+    // Makes 'process' globally available in the browser, which some libraries expect.
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      })
+    );
     
     return config;
   },
