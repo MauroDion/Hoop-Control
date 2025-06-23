ef6274cf14d3789ae7c93991307da7fff86ee04c
Mauro Dion Alonso Olmo <maurodion@gmail.com>
2025-06-23 06:44:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -17,6 +17,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
