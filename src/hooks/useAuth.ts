980a67197f871df50b315b7e2b3d8501d65eff59
Mauro Dion Alonso Olmo <maurodion@gmail.com>
2025-07-02 18:51:04 +0000

    en el code ha salido esto!!!!                   Warning: Cannot update a

--- src/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-// This file is deprecated and will be removed in a future update.
-// Please use `import { useAuth } from '@/contexts/AuthContext';` instead.
-import { useAuth as useAuthFromContext } from '@/contexts/AuthContext';
-
-export const useAuth = () => {
-  return useAuthFromContext();
-};
