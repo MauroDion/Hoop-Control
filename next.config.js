d9b78f36d45bf659ec23ffaa7f34f95e05a83af0
Mauro Dion Alonso Olmo <maurodion@gmail.com>
2025-07-13 19:38:44 +0000

    algo ma tiene qeu haber, mira despues de darle a Reinciar APP como sale

--- next.config.js
+++ next.config.js
@@ -9,28 +9,6 @@ const nextConfig = {
             },
         ],
     },
-    webpack: (config, { isServer }) => {
-        // This is to polyfill `process` which is not available in the browser.
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                process: require.resolve('process/browser'),
-            };
-
-            config.plugins.push(
-                new (require('webpack').ProvidePlugin)({
-                    process: 'process/browser',
-                })
-            );
-        }
-        
-        // Exclude server-side packages from the bundle for both client and server.
-        // This prevents Webpack from trying to parse native Node.js addons like `farmhash.node`
-        // which are dependencies of `firebase-admin`.
-        config.externals.push('firebase-admin', 'farmhash');
-        
-        return config;
-    },
 };
 
 module.exports = nextConfig;
